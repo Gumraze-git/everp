2e7adc0e485102e53534a17980f3b21ad1b3cb70
gumraze-git <galaxydh4110@gmail.com>
2025-10-29 04:22:19 +0900

    feat(auth): 내부 사용자 인증 계정 생성 및 결과 이벤트 추가 (#14)
    - `CreateAuthUserEvent` 클래스 추가: 사용자 계정 생성 이벤트 정의.
    - `CreateAuthUserResultEvent` 클래스 추가: 생성 결과 이벤트 정의 및 성공/실패 데이터 포함.
    - saga 흐름에서 사용자 인증 계정 관련 이벤트 처리 추가 지원.

---
+++ src/main/java/org/ever/event/CreateAuthUserEvent.java
@@ -0,0 +1,23 @@
+package org.ever.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAuthUserEvent {
+    // Kafka
+    private String eventId;             // 카프카 브로커에 발행되는 이벤트 자체의 고유 식별자
+    private String transactionId;       // saga 코레오그래피 흐름에서 공통으로 사용되는 트랜잭션 상관 키
+    private boolean success;            // 해당 이벤트가 성공인지 실패 및 보상 흐름인지 나타내는 flag
+
+    // 카프카 이벤트로 전송할 데이터
+    private String userId;
+    private String email;
+    private String positionName;
+    private String departmentName;
+}
